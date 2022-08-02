eb6aff84bc53127dc0d623a78dc165a81a3fb5ed
Pavan0409 <pavanchitaguppa@gmail.com>
2022-08-03 01:16:49 +0530

    delete item

--- src/App.js
+++ src/App.js
@@ -1,41 +1,82 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 
+// const App = () => {
+//   const expenses = [
+//     {
+//       id: "e1",
+//       title: "Perfume",
+//       amount: 100.23,
+//       date: new Date(2022, 4, 12),
+//       LocationOfExpenditure: "Punjagutta",
+//     },
+//     {
+//       id: "e2",
+//       title: "Chpati roller",
+//       amount: 129,
+//       date: new Date(2022, 5, 10),
+//       LocationOfExpenditure: "Ameerpet",
+//     },
+//     {
+//       id: "e3",
+//       title: "Chicken party",
+//       amount: 259.69,
+//       date: new Date(2022, 6, 22),
+//       LocationOfExpenditure: "Banjara hills",
+//     },
+//     {
+//       id: "e4",
+//       title: "Agarbatti",
+//       amount: 20.23,
+//       date: new Date(2022, 7, 19),
+//       LocationOfExpenditure: "Jublee hills",
+//     },
+//   ];
+
+//   return (
+//     <div>
+//       {/* <h2>let's get started!</h2> */}
+//       <Expenses item={expenses} />
+//     </div>
+//   );
+// };
+
+
+
 const App = () => {
-  const expenses = [
+  const deleteHandler = (amount) => {
+    console.log(amount);
+    expenses = data.filter((item) => item.amount !== amount);
+    setData(expenses);
+    console.log(expenses);
+  };
+
+  let expenses = [
     {
       id: "e1",
-      title: "Perfume",
-      amount: 100.23,
-      date: new Date(2022, 4, 12),
-      LocationOfExpenditure: "Punjagutta",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
     },
-    {
-      id: "e2",
-      title: "Chpati roller",
-      amount: 129,
-      date: new Date(2022, 5, 10),
-      LocationOfExpenditure: "Ameerpet",
-    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
-      title: "Chicken party",
-      amount: 259.69,
-      date: new Date(2022, 6, 22),
-      LocationOfExpenditure: "Banjara hills",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
-      title: "Agarbatti",
-      amount: 20.23,
-      date: new Date(2022, 7, 19),
-      LocationOfExpenditure: "Jublee hills",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
   ];
-
+  const [data, setData] = useState(expenses);
   return (
     <div>
-      {/* <h2>let's get started!</h2> */}
-      <Expenses item={expenses} />
+      <h2>Let's get started!</h2>
+      <Expenses items={data} deleteHandler={deleteHandler} />
     </div>
   );
 };
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+// const ExpenseItem = (props) => {
+//   const [title, setTitle]  = useState(props.title);
+
+//   const clickHandler = () => {
+//     setTitle("Updated!");
+//     console.log(title)
+//   };
+
+//   return (
+//     <Card className='expense-item'>
+//       <ExpenseDate date={props.date} />
+//       <div className='expense-item__description'>
+//         <h2>{title}</h2>
+//         <div className='expense-item__price'>${props.amount}</div>
+//       </div>
+//       <button onClick={clickHandler}>Change title</button>
+//     </Card>
+//   );
+// }
 
 const ExpenseItem = (props) => {
+  const clickHandler = (id) => {
+    props.deleteHandler(props.amount);
+  };
+
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Delete Expenses</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
