6b611341720b4cf7c7ef23677069d0a696aa5750
Pavan0409 <pavanchitaguppa@gmail.com>
2022-08-04 01:03:31 +0530

    powering submit button

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -19,12 +19,29 @@ const ExpenseForm = () => {
     console.log(event.target.value);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmounnt,
+      Date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setenteredAmount("");
+    setenteredTitle("");
+    setenteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>title</label>
-          <input type="text" onChange={titleChangeHandler}></input>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          ></input>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -33,6 +50,7 @@ const ExpenseForm = () => {
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
+            value={enteredAmounnt}
           ></input>
         </div>
         <div className="new-expense__control">
@@ -42,6 +60,7 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
+            value={enteredDate}
           ></input>
         </div>
         <button type="submit">Add Expense</button>
