a5904c512db4828d38f6ca6656e747f8745f5a52
Pavan0409 <pavanchitaguppa@gmail.com>
2022-08-05 18:33:04 +0530

    rendering list

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-import React from "react";
-import Expenses from "./components/Expenses/Expenses";
+import React, { useState } from "react";
+
 import NewExpense from "./components/ExpenseForm/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Perfume",
-      amount: 100.23,
-      date: new Date(2022, 4, 12),
-      LocationOfExpenditure: "Punjagutta",
-    },
-    {
-      id: "e2",
-      title: "Chpati roller",
-      amount: 129,
-      date: new Date(2022, 5, 10),
-      LocationOfExpenditure: "Ameerpet",
-    },
-    {
-      id: "e3",
-      title: "Chicken party",
-      amount: 259.69,
-      date: new Date(2022, 6, 22),
-      LocationOfExpenditure: "Banjara hills",
-    },
-    {
-      id: "e4",
-      title: "Agarbatti",
-      amount: 20.23,
-      date: new Date(2022, 7, 19),
-      LocationOfExpenditure: "Jublee hills",
-    },
-  ];
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
-      <h2>let's get started!</h2>
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses item={expenses} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,51 +1,31 @@
-import React, { useState } from "react";
-import ExpensesFilter from "./ExpensesFilter";
+import React, { useState } from 'react';
 
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
-import Card from "../UI/Card";
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
   return (
     <div>
-      <Card className="expenses">
+      <Card className='expenses'>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.item.map((item) => (
+        {props.items.map((expense) => (
           <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-            LocationOfExpenditure={item.LocationOfExpenditure}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
           />
         ))}
-
-        {/* <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-        LocationOfExpenditure={props.item[1].LocationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-        LocationOfExpenditure={props.item[2].LocationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-        LocationOfExpenditure={props.item[3].LocationOfExpenditure}
-      /> */}
       </Card>
     </div>
   );
