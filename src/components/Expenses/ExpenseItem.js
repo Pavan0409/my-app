72020b6db3e2ba212a1adaed47213acb3d3639ef
Pavan0409 <pavanchitaguppa@gmail.com>
2022-08-06 17:42:05 +0530

    conditional content

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,19 @@
-import React, { useState } from "react";
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [amount, setAmount] = useState(props.amount);
-
-  const clickHandler = () => {
-    setAmount("100");
-    console.log(amount + 100);
-  };
-
   return (
-    <Card className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h4>{props.LocationOfExpenditure}</h4>
-        <h1>{props.title}</h1>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">₹{amount}</div>
-      <button onClick={clickHandler}>Change amount</button>
+      <div className='expense-item__price'>₹{props.amount}</div>
     </Card>
   );
-};
+}
 
 export default ExpenseItem;
